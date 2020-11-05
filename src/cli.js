0b9a479fcb219b726357aa4bd87f9bf0f91c5724
Bogdan Pugachuk <intheend54@gmail.com>
2020-11-05 22:59:15 +0500

    Update Makefile, package.json & cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import promptly from 'promptly';
 
 export default async () => {
-    const name = await promptly.prompt('May I have your name?');
+    const name = await promptly.prompt('May I have your name?', { silent: true });
     console.log(`Hello, ${name}!`);
 };
