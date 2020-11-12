6e03339c658b89fa3624fb68c643b83e8ed5f374
Bogdan Pugachuk <intheend54@gmail.com>
2020-11-12 17:58:05 +0500

    Another Testing

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+import promptly from 'promptly';
+
+(async () => {
+    console.log('Welcome to the Brain Games!');
+    const userName = await promptly.prompt('May I have your name?', { silent: true });
+    console.log(`Hello, ${userName}!`);
+})();
