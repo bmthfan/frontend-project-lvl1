6322452c2f03397feda74986d44f050367a4e280
Bogdan Pugachuk <intheend54@gmail.com>
2020-11-14 16:35:20 +0500

    Update game-engine.js

--- src/game-engine.js
+++ src/game-engine.js
@@ -1,9 +1,39 @@
 import promptly from 'promptly';
 import { car, cdr } from '@hexlet/pairs';
+import { calcGame, getCalcQuestionsAnswers } from './games/calc.js';
+import { evenGame, getEvenQuestionsAnswers } from './games/even.js';
+import { gcdGame, getGcdQuestionsAnswers } from './games/gcd.js';
+import { primeGame, getPrimeQuestionsAnswers } from './games/prime.js';
+import { progressionGame, getProgressionQuestionsAnswers } from './games/progression.js';
 
-export const getRandomNumberBetween = (min, max) => Math.floor(Math.random() * (max - min) + min);
+let questionsAnswers;
+let gameName;
 
-export const runGame = async (gameName, questionsAnswers) => {
+export default async (game) => {
+  switch (game) {
+    case 'calc':
+      questionsAnswers = getCalcQuestionsAnswers();
+      gameName = calcGame;
+      break;
+    case 'even':
+      questionsAnswers = getEvenQuestionsAnswers();
+      gameName = evenGame;
+      break;
+    case 'gcd':
+      questionsAnswers = getGcdQuestionsAnswers();
+      gameName = gcdGame;
+      break;
+    case 'prime':
+      questionsAnswers = getPrimeQuestionsAnswers();
+      gameName = primeGame;
+      break;
+    case 'progression':
+      questionsAnswers = getProgressionQuestionsAnswers();
+      gameName = progressionGame;
+      break;
+    default:
+      break;
+  }
   console.log('Welcome to the Brain Games!');
   const userName = await promptly.prompt('May I have your name?', { silent: true });
   console.log(`Hello, ${userName}!\n${gameName}`);
@@ -11,19 +41,17 @@ export const runGame = async (gameName, questionsAnswers) => {
   for (let i = 0; i <= questionsAnswers.length; i += 1) {
     const questionAnswer = questionsAnswers[i];
     const userAnswer = await promptly.prompt(`Question: ${car(questionAnswer)}`, { silent: true, default: '' });
-    const wrongAnswer = `'${userAnswer}' is wrong answer ;(. Correct answer was '${cdr(questionAnswer)}'. \nLet's try again, ${userName}!`;
-    const lastQuestion = questionsAnswers.length - 1;
 
     console.log('Your answer:', userAnswer);
-    if (userAnswer === String(cdr(questionAnswer))) {
+    if (userAnswer === cdr(questionAnswer)) {
       console.log('Correct!');
-      if (questionAnswer === questionsAnswers[lastQuestion]) {
+      if (i === questionsAnswers.length - 1) {
         console.log(`Congratulations, ${userName}!`);
-        break;
+        return;
       }
     }
-    if (userAnswer !== String(cdr(questionAnswer))) {
-      console.log(wrongAnswer);
+    if (userAnswer !== cdr(questionAnswer)) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${cdr(questionAnswer)}'. \nLet's try again, ${userName}!`);
       break;
     }
   }
